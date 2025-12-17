fdd7a17789b5743649df28416f2c4ccaef7d1564
Cameron Philip Carter <cameron@gofundraise.com>
2025-12-18 00:53:03 +1100

    laptop fix

--- app/components/sections/hero/HeroSection.tsx
+++ app/components/sections/hero/HeroSection.tsx
@@ -3,7 +3,7 @@ import Button from '@/app/components/shared/Button';
 
 export default function HeroSection() {
   return (
-    <section className="relative h-screen flex items-center justify-center px-5 md:px-8 lg:px-16 py-0 pt-[72px] overflow-hidden">
+    <section className="relative min-h-screen flex items-center justify-center px-5 md:px-8 lg:px-16 py-0 pt-[72px] overflow-hidden">
       {/* Video Background */}
       <video
         autoPlay
