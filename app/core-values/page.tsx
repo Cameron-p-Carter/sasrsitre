d4b18ef237a1f104810433edffdac9a712eb65f0
Cameron Philip Carter <cameron@gofundraise.com>
2025-12-19 01:34:24 +1100

    dark

--- app/core-values/page.tsx
+++ app/core-values/page.tsx
@@ -12,17 +12,17 @@ const SECOND_CORE_VALUES_DATA = {
   description: "Our values define our culture and are the foundation of our success. They guide our decisions, shape our interactions, and inspire us to achieve excellence together.",
   values: [
     {
-      icon: "/images/icons/speed.svg",
+      icon: "/images/icons/speed_dark.svg",
       title: "Boost: Increasing velocity and productivity.",
       description: "Uplifting capabilities, processes and practices to build high performing teams that have the scale to meet market demands and stay ahead of the competition. We lift teams higher. We’re here to make an impact. That means helping every team we work with perform at their best - faster, smarter, and stronger. We share knowledge, refine processes, and bring energy that inspires growth. When we boost others, we all rise together. ",
     },
     {
-      icon: "/images/icons/block.svg",
+      icon: "/images/icons/block_dark.svg",
       title: "Unblock: Removing obstacles to scale.",
       description: "By unblocking barriers, we ensure that projects can proceed smoothly and efficiently, allowing teams to focus on business growth, innovation and delivering value. We clear the way. Complex problems don’t intimidate us - they motivate us. When things get stuck, we lean in, ask the right questions, and find a path forward. We simplify, collaborate, and get things moving again, because progress doesn’t happen by waiting - it happens by unblocking. ",
     },
     {
-      icon: "/images/icons/grading.svg",
+      icon: "/images/icons/grading_dark.svg",
       title: "Safeguard: Ensuring data security and integrity.",
       description: "We safeguard critical data implementing robust security measures to protect against potential threats. We build with care. We take pride in doing things right. Our work protects data, systems, and people - and we treat that responsibility seriously. From secure code to reliable delivery, we safeguard the integrity of what we build. Quality and trust are never negotiable. ",
     },
